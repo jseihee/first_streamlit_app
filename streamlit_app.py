46812ce9fae6b195742745ab0ba1c844ce9a8ed6
Seihee <90475328+jseihee@users.noreply.github.com>
2023-02-05 17:25:51 +0000

    Update streamlit_app.py
    Added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Chia pudding with maple syrup')
+streamlit.text('Energizing green goddess smoothie')
